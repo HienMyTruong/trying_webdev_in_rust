28b339a21a18dffa7b8122e47918913586d50263
Hien Truong <hien.truong2002.ht@gmail.com>
2023-01-18 22:43:08 +0100

    playing around with different states

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,7 @@
+use std::rc::Rc;
 use yew::prelude::*;
 
+/*
 #[function_component]
 fn App() -> Html {
     let counter = use_state(|| 0);
@@ -13,10 +15,57 @@ fn App() -> Html {
 
     html! {
         <div>
-            <button {onclick}>{ "+1" }</button>
-            <div>{"Hejsan svejsan"}</div>
+            <button {onclick} class="theme_dark">{"Dank mode"}</button>
+            <div>{"Hej ling"}</div>
             <p>{ *counter }</p>
         </div>
+    }
+}
+*/
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Theme {
+    gamerness: Gamerness,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Gamerness {
+    DarkMode,
+    NoobMode,
+}
+
+#[function_component]
+fn NavButton() -> Html {
+    let theme = use_context::<Rc<Theme>>().expect("Failed to get context");
+
+    let color = match theme.gamerness {
+        Gamerness::DarkMode => "blue",
+        Gamerness::NoobMode => "yellow",
+    }
+    .to_string();
+
+    //let onclick = { color };
+
+    html! {
+        <div style={format!("background-color: {}", color)}>{"hejsan svejsan"}</div>
+        /*<div>
+            <button {onclick} class="theme_dark">{"Dank mode"}</button>
+            <div>{"Hej ling"}</div>
+            <p>{ color }</p>
+        </div>*/
+    }
+}
+
+#[function_component]
+fn App() -> Html {
+    let theme = use_state(|| Theme {
+        gamerness: Gamerness::NoobMode,
+    });
+
+    html! {
+        <ContextProvider<Rc<Theme>> context={Rc::new(*theme)}>
+            <NavButton />
+        </ContextProvider<Rc<Theme>>>
     }
 }
 
